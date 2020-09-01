8ddf7bdf09dbd021737cc45c2e7d44ac6d3efbd5
Hadi Tedi <haditedi@gmail.com>
2020-09-01 13:13:18 +0100

    added form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Input, Button } from 'antd'
 
 const Contact = () => {
+  const [state, setState] = useState({ name: '' })
+  console.log(state)
   const handleChange = e => {
+    const { name, value } = e.target
+    setState(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value,
+      }
+    })
     console.log(e.target.name, e.target.value)
   }
   const handleSubmit = e => {
-    e.preventDefault()
-    console.log(e.target.value)
+    console.log(state)
   }
   return (
     <div>
       <h1>Contact Us</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        name="contactForm"
+        action="/success"
+        method="POST"
+        data-netlify="true"
+      >
+        <input type="hidden" name="form-name" value="contactForm"></input>
         <Input
           placeholder="Name"
           onChange={handleChange}
           name="name"
+          value={state.name}
           required
         />
         <Button htmlType="submit">Submit</Button>
